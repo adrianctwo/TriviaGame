b8d11e3ff309698b3ef4f32a1fa3e2b979070eca
adrianctwo <adrian.e.situ@gmail.com>
2019-06-27 14:13:24 -0700

    Added question as object to javascript

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+var Questions = [{
+    question: "What is my name?",
+    answerChoices: ["A", "B", "C", "D"],
+    answer: 0
+},{
+    question: "what is my last name?",
+    answerChoices: ["A", "B", "C", "S"],
+    answer: 3
+}]
+
